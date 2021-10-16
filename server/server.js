9316ee17230dabb305056aeb598c518334658d7a
Dan <bmdmartinez@gmail.com>
2021-10-16 16:48:00 -0400

    created auth middleware to accept graphql and implemented it in server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// importing apolloserver
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers} = require('./schemas');
+const { authMiddleware, authMiddleware } = require('./utils/auth');
 
+// creating express server
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// creating apollo server
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  })
+
+  // Start the Apollo server
+  await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +36,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+  });
 });
