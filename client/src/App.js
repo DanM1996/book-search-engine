7413491dc78f8c089ce0982cab138186463fd400
Dan <bmdmartinez@gmail.com>
2021-10-16 17:44:30 -0400

    added apollo provider to app.js so requests work

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context'; 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+// the underscore serveres as a placeholder for a parameter that we can skip. we don't need the data, but it needs a placeholder to access the second one
+const authLink = setContext((_, { headers }) => {
+  // gets user specific token from local storage
+  const token = localStorage.getItem('id_token');
+  return {
+    // ever headers request will now incorporate the token to make sure the session is valid/active
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // combines the http and authlink so that every http request includes the auth token
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +42,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
