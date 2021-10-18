25443fbc8c4f5c354d04f9d63f84f89d5a60d7d5
Dan <bmdmartinez@gmail.com>
2021-10-17 21:33:51 -0400

    app fully working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,10 +5,18 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
+            console.log(context.user);
             if (context.user) {
-                const userData = await User.findOne({})
-                    .select('-__v - password')
-                return userData
+                try {
+                    const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    console.log("userData:", userData);
+                    return userData
+                } catch(err) {
+                    console.log(err);
+                }
+                
+                
             }
             throw new AuthenticationError('Not logged in');
         }
